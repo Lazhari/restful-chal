40f297598e06a16a6522bf925c0f072a72b27585
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-05 11:22:52 +0000

    Add images end points to global route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const config = require('../config/enviroment');
+
+const APIROOT = `/api/${config.apiVersion}`;
+
 module.exports = function(app) {
-    app.use('/', require('./app'));
+    app
+        .use('/', require('./app'))
+        .use(`${APIROOT}/images`, require('./api/images'));
 };
