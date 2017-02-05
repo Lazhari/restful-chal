1bfe3dd759fc8e87812dad6fd76a619c31cec558
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-05 12:53:57 +0000

    Create a script that generate fakes images

---
+++ mocks/images.js
@@ -0,0 +1,37 @@
+'use strict';
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const mongoose = require('mongoose');
+const faker = require('faker');
+const async = require('async');
+
+const config = require('../config/enviroment');
+const Image = require('../models/image');
+
+/**
+* Create new fake fashion image
+*/
+const createFakeImage = function(iteration, done) {
+    const product =  {
+        url: faker.image.fashion()
+    };
+    return done(null, product);
+};
+
+mongoose.connect(config.mongo.uri, (err) => {
+    if(err) {
+        console.error(`Connection to database not established, please check your MongoDB connection maybe the reason is ${err.message}`);
+    } else {
+        async.times(50, async.apply(createFakeImage), function(err, images) {
+            Image.insertMany(images, (err) => {
+                if(err) {
+                    console.error('Something went wrong');
+                } else {
+                    console.log('Images has been added to database');
+                }
+                process.exit(1);
+            });
+        });
+    }
+});
