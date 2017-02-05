359cdf5ae5eb6c9a3d6225e3a1b30d2c0fc4c053
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-05 14:43:42 +0000

    Add items end point module to the global router

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ module.exports = function(app) {
     app
         .use('/', require('./app'))
         .use(`${APIROOT}/images`, require('./api/images'))
-        .use(`${APIROOT}/products`, require('./api/products'));
+        .use(`${APIROOT}/products`, require('./api/products'))
+        .use(`${APIROOT}/items`, require('./api/items'));
 };
