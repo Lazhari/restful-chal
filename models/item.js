af3d17fa4e55f95589f12a5138e5ef3599002d9c
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-05 10:48:26 +0000

    Create the Item model schema

---
+++ models/item.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const ItemSchema = new Schema({
+    product_id: {
+        type: ObjectId,
+        red: 'Product'
+    },
+    product_name: String,
+    product_sku: String,
+    size: String,
+    color: {
+        type: ObjectId,
+        ref: 'Color'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date
+});
+
+ItemSchema.pre('update', function() {
+    this.update({}, {
+        $set: {
+            updated: new Date()
+        }
+    });
+});
+
+module.exports = mongoose.model('Item', ItemSchema, 'items');
