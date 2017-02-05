181fdbb2f7d0f876a85961bb24c14371ad49d86a
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-05 09:48:02 +0000

    Refactoring the express application

--- app.js
+++ app.js
@@ -1,69 +1,11 @@
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const expressNunjucks = require('express-nunjucks');
-
-const routes = require('./routes/index');
-const users = require('./routes/users');
-
 const app = express();
-// Check the environment if is dev
-const isDev = app.get('env') === 'development';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// Config nunjuncks express engine template
-const njk = expressNunjucks(app, {
-    watch: isDev,
-    noCache: isDev
-});
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error.html', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error.html', {
-    message: err.message,
-    error: {}
-  });
-});
-
+require('./config/express')(app);
 
 module.exports = app;
---
+++ config/express.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressNunjucks = require('express-nunjucks');
+
+const config = require('./enviroment');
+
+var basicAuth = require('basic-auth');
+
+module.exports = function (app) {
+
+    // view engine setup
+    // This is where all the magic happens!
+    // Check the environment if is dev
+    const isDev = app.get('env') === 'development';
+
+    // view engine setup
+    app.set('views', path.join(config.root, './views'));
+    // Config nunjuncks express engine template
+    const njk = expressNunjucks(app, {
+        watch: isDev,
+        noCache: isDev
+    });
+
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    app.use(express.static(path.join(__dirname, '../public')));
+    // TODO generate API doc and module doc
+    //app.use('/doc', auth, express.static(path.join(__dirname, '../api-doc')));
+    //app.use('/modules-doc', auth, express.static(path.join(__dirname, '../modules-doc')));
+
+    // Routing Systems
+    require('../routes')(app);
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handlers
+
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use('/:url(api|auth)/*', function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.send({
+                message: err.message,
+                error: err.stack
+            });
+        });
+        app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use('/:url(api|auth)/*', function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.send(err);
+    });
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+};
